0b19723c2cf8ac40b531de0aa84009600329a167
RobynHancock <u19068035@tuks.co.za>
2024-05-02 14:01:28 +0200

    Added Firebase Authorisation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  username: string ='';
+export class LoginComponent implements OnInit {
+  email: string ='';
   password: string = '';
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void{
     throw new Error('Method not implemented');
   }
 
   login(){
-    if(this.username == '')
+    if(this.email == '')
       {
-        alert("Please Enter Your Username");
+        alert("Please Enter Your Email");
         return;
       }
-      if(this.password == '')
-        {
-          alert("Please Enter Your Password");
-          return;
-        }
+    if(this.password == '')
+      {
+        alert("Please Enter Your Password");
+        return;
+      }
+    
+    this.auth.login(this.email, this.password);
+    this.email = '';
+    this.password = '';
+
   }
 }

80d24e4693c6ccf2428c79332eb291028d22552d
RobynHancock <u19068035@tuks.co.za>
2024-05-14 14:17:03 +0200

    Updated Environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCISjdlfmvbQ0mm2Ve_l1opISEWMg1kL2Q",
+        authDomain: "cos720-project.firebaseapp.com",
+        databaseURL: "https://cos720-project-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "cos720-project",
+        storageBucket: "cos720-project.appspot.com",
+        messagingSenderId: "117044789101",
+        appId: "1:117044789101:web:34f2a518186e186bd6a464",
+        measurementId: "G-168QHLYMQN"
+    },
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
     firebase: {
         apiKey: "AIzaSyCISjdlfmvbQ0mm2Ve_l1opISEWMg1kL2Q",
         authDomain: "cos720-project.firebaseapp.com",
+        databaseURL: "https://cos720-project-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "cos720-project",
         storageBucket: "cos720-project.appspot.com",
         messagingSenderId: "117044789101",
